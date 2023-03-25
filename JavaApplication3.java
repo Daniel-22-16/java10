24b0a740919b6d467cbaa7db0bb9923531c73b85
Daniel-22-16 <1475937@senati.pe>
2023-03-25 08:59:19 -0500

    java10

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -10,5 +10,6 @@ public class JavaApplication3 {
         System.out.println("\t\t\t you are great");
         System.out.println("\t\t this is my pleasure");
         System.out.println("\t no,..... you are welcome");
+        
     }
 }
