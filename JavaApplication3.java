161d7b23742f2e906e89df255cb47359598c8561
Daniel-22-16 <1475937@senati.pe>
2023-03-25 08:20:48 -0500

    java10

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -5,7 +5,7 @@ public class JavaApplication3 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-
+        System.out.println("Morning, Morning, Morning");
     }
 }
     
